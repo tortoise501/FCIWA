eee55e46f73d0b7667d7f5c858955e44b070310c
tortoise501 <46166293+tortoise501@users.noreply.github.com>
2023-10-31 09:45:12 +0100

    Fixing localization bug with css

--- FCIWA/Program.cs
+++ FCIWA/Program.cs
@@ -2,6 +2,9 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using FCIWA;
 
+CultureInfo.DefaultThreadCurrentCulture = CultureInfo.InvariantCulture;
+CultureInfo.DefaultThreadCurrentUICulture = CultureInfo.InvariantCulture;
+
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
